654c5d627c9c34ddbb73d509a282a12993201106
RadostDudhi <rudakova1720@gmail.com>
2022-08-20 20:44:50 +0300

    add home page

---
+++ src/components/Message/index.js
@@ -0,0 +1,37 @@
+import React from "react";
+import PropTypes from 'prop-types';
+import classNames from "classnames";
+import format from 'date-fns/format';
+
+import './Message.scss';
+
+
+const Message = ({avatar, user, text, date, isMe}) => (
+    <div className={classNames('message', {'message--isme': isMe})}>
+        <div>
+            <img className="message__avatar" src={avatar} alt={`Avatar ${user.fullname}`} />
+        </div> 
+        <div className="message__content">
+            <div className="message__bubble">
+                <p className="message__text">{text}</p>
+            </div>
+            <span className="message__date">
+                {format(new Date(date), 'Pp')}
+            </span>
+        </div>
+    </div>
+    
+)
+
+Message.defaultProps = {
+    user: {}
+};
+
+Message.propTypes = {
+    avatar: PropTypes.string,
+    text: PropTypes.string,
+    date: PropTypes.string,
+    user: PropTypes.object
+};
+
+export default Message;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-import {
-    Routes,
-    Route
-  } from 'react-router-dom';
+//import {
+ //   Routes,
+ //   Route
+  //} from 'react-router-dom';
+import { Message } from "../../components";
 
 
 import './Home.scss';
@@ -10,7 +11,17 @@ import './Home.scss';
 const Home = () => ( 
     
     <section className="home">
-            <h2>Hello</h2>
+        <Message 
+        avatar="https://sun6-23.userapi.com/s/v1/if1/SrG0dzIWKMmFcc_b2xw5S9JsKqz6mnt9_FuHwma0bOt2Qv9uvOhT6NrlGmLDlxvkqeKTie8D.jpg?size=100x100&quality=96&crop=321,1,1916,1916&ava=1" 
+        text="Quickly come to the meeting room 1B, we have a big server issue" 
+        date="Sat Aug 20 2022 19:37:12"  
+        />
+        <Message 
+        avatar="https://sun6-21.userapi.com/s/v1/if1/e9qP6NNivkqG4rRkEr3ynauZlOYr-N8HOnEKlI5M8ye7P88_j7BcbKu0Dj-PeWBPe1OHni02.jpg?size=100x100&quality=96&crop=0,97,1104,1104&ava=1" 
+        text="I'm having breakfast right now, can't you wait for 10 minutes?" 
+        date="Sat Aug 20 2022 19:45:10" 
+        isMe={true}
+        />
     </section>
     
 );
